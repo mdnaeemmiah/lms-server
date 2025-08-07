b11680e496633ca54e97892e4a56a96b5dba27c1
mdnaeemmiah <mdnaeemmiah48@gmail.com>
2025-08-08 05:56:13 +0600

    added

---
+++ dist/routes/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const auth_route_1 = __importDefault(require("../app/modules/auth/auth.route"));
+const user_route_1 = __importDefault(require("../app/modules/user/user.route"));
+const router = (0, express_1.Router)();
+const moduleRoutes = [
+    {
+        path: '/auth',
+        route: auth_route_1.default,
+    },
+    {
+        path: '/user',
+        route: user_route_1.default,
+    },
+];
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+exports.default = router;
